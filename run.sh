e7602a94996ddad5aae23d5a597cb340995eb85f
유우진 <ywj02120212@gmail.com>
2025-12-20 09:18:30 +0900

    chore: restructure source directory

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 source /Users/ywj0212/Desktop/coding/yonhwa-font/venv/bin/activate
-export PYTHONPATH="$PWD/venv/lib/python3.14/site-packages:$PYTHONPATH"
-fontforge -lang=py -script ./main.py
+export PYTHONPATH="$PWD/src:$PWD/venv/lib/python3.14/site-packages:$PYTHONPATH"
+fontforge -lang=py -script ./src/main.py
