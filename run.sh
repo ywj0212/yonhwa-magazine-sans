2b3e164f8fd9506c829ab85d1ee86e7b9577c343
유우진 <ywj02120212@gmail.com>
2025-12-20 09:20:42 +0900

    fix: build script venv path

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-source /Users/ywj0212/Desktop/coding/yonhwa-font/venv/bin/activate
+source ./venv/bin/activate
 export PYTHONPATH="$PWD/src:$PWD/venv/lib/python3.14/site-packages:$PYTHONPATH"
 fontforge -lang=py -script ./src/main.py
